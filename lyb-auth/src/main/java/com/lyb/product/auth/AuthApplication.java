467282529c8a23dea88201450ef99bfb2f7f5d44
xiexiangrui <xie_xiangrui@163.com>
2018-04-09 18:27:38 +0800

    auth

---
+++ lyb-auth/src/main/java/com/lyb/product/auth/AuthApplication.java
@@ -0,0 +1,21 @@
+package com.lyb.product.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+/**
+ * @program: lyb-product
+ * @description:
+ * @author: xiexiangrui
+ * @create: 2018-04-09 18:15
+ **/
+@SpringBootApplication
+@EnableAutoConfiguration()
+@EnableResourceServer
+public class AuthApplication {
+    public static void main(String[] args){
+        SpringApplication.run(AuthApplication.class, args);
+    }
+}

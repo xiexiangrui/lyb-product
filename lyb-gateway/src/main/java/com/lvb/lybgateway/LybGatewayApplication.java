79869bc68d3f51a5378ad2e6acbc00c4cab6680d
xiexiangrui <xie_xiangrui@163.com>
2018-04-10 18:41:08 +0800

    登录页面

--- lyb-gateway/src/main/java/com/lvb/lybgateway/LybGatewayApplication.java
+++ lyb-gateway/src/main/java/com/lvb/lybgateway/LybGatewayApplication.java
@@ -2,8 +2,10 @@ package com.lvb.lybgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class LybGatewayApplication {
 
     public static void main(String[] args) {
